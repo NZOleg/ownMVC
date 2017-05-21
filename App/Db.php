8f18b75001759b8e0ba5d489b9db2c2cb1a30d64
Oleg Nesterov <NZOlegok@gmail.com>
2017-05-21 14:21:12 +1200

    +Autoload

---
+++ App/Db.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: NZOle
+ * Date: 21/05/2017
+ * Time: 2:17 PM
+ */
+
+namespace App;
+
+
+class Db
+{
+    public function __construct()
+    {
+        echo 'Hello Db';
+    }
+}
--- index.php
+++ index.php
@@ -1,3 +1,4 @@
 <?php
 
-echo "Hello world";
+require __DIR__. '/autoload.php';
+$db = new \App\Db();
