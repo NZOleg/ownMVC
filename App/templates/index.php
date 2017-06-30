9ed12e7c1323087f82c39a9a9078bc6ffbbb2715
Oleg Nesterov <NZOlegok@gmail.com>
2017-06-30 17:18:50 +1200

    + setting view

--- App/templates/index.php
+++ App/templates/index.php
@@ -10,7 +10,12 @@
 </head>
 <body>
 <h1>Hello, world!</h1>
-
+<?php foreach ($users as $user) :?>
+<div class="card">
+    <div class=".card-header"><?php echo $user->name; ?></div>
+    <div class="card-block"><?php echo $user->email; ?></div>
+</div>
+<?php endforeach; ?>
 <!-- jQuery first, then Tether, then Bootstrap JS. -->
 <script src="https://code.jquery.com/jquery-3.1.1.slim.min.js" integrity="sha384-A7FZj7v+d/sdmMqp/nOQwliLvUsJfDHW+k9Omg/a/EheAdgtzNs3hpfag6Ed950n" crossorigin="anonymous"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/tether/1.4.0/js/tether.min.js" integrity="sha384-DztdAPBWPRXSA/3eYEEUWrWCy7G5KFbe8fFjk5JAIxUYHKkDx6Qin1DkWx51bBrb" crossorigin="anonymous"></script>
